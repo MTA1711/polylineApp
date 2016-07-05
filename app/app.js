c3260ccc93850cc106601c663bfe6beaac453cfe
MTA1711 <mongaachille@gmail.com>
2016-07-05 18:39:04 +0200

    Add features: Draw POI, Draw PolyLine

--- app/app.js
+++ app/app.js
@@ -7,9 +7,26 @@ app.module = angular.module('app', ['ngeo']);
 
 
 /**
+ * @param {!angular.Scope} $scope Angular scope.
+ * @param {ol.Collection.<ol.Feature>} ngeoFeatures Collection of features.
+ * @param {ngeo.ToolActivateMgr} ngeoToolActivateMgr Ngeo ToolActivate manager
+ *     service.
  * @constructor
  */
-app.MainController = function() {
+app.MainController = function($scope, ngeoFeatures, ngeoToolActivateMgr) {
+
+  /**
+   * @type {!angular.Scope}
+   * @private
+   */
+  this.scope_ = $scope;
+
+  var vector = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      wrapX: false,
+      features: ngeoFeatures
+    })
+  });
 
   /**
    * @type {ol.Map}
@@ -19,13 +36,32 @@ app.MainController = function() {
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
-      })
+      }),
+      vector
     ],
     view: new ol.View({
       center: [0, 0],
-      zoom: 4
+      zoom: 3
     })
   });
+
+  /**
+   * @type {boolean}
+   * @export
+   */
+  this.drawActive = false;
+
+  var drawToolActivate = new ngeo.ToolActivate(this, 'drawActive');
+  ngeoToolActivateMgr.registerTool('mapTools', drawToolActivate, false);
+
+  /**
+   * @type {boolean}
+   * @export
+   */
+  this.dummyActive = true;
+
+  var dummyToolActivate = new ngeo.ToolActivate(this, 'dummyActive');
+  ngeoToolActivateMgr.registerTool('mapTools', dummyToolActivate, true);
 };
 
 
