4b960efac91f1e9526d84a9d95550d90e5a61617
MTA1711 <mongaachille@gmail.com>
2016-07-06 18:42:48 +0200

    Add new features : Export POI and POLYLines in GeoJSON format

--- app/app.js
+++ app/app.js
@@ -13,12 +13,35 @@ app.module = angular.module('app', ['ngeo']);
  *     service.
  * @constructor
  */
-app.MainController = function($scope, ngeoFeatures, ngeoToolActivateMgr) {
+app.MainController = function($scope, $document, ngeoFeatures, ngeoToolActivateMgr) {
+  
+  /**
+   * @private
+   * @type {Document}
+   */
+  this.$document_ = $document;
+
+  /**
+   * @private
+   * @type {ol.format.KML}
+   */
+  this.kmlFormat_ = new ol.format.KML();
+
+  /**
+   * @private
+   * @type {ol.format.GeoJSON}
+   */
+  this.geoJsonFormat_ = new ol.format.GeoJSON();
 
   /**
    * @type {!angular.Scope}
    * @private
    */
+
+  this.features = null;
+  console.log("test");
+
+
   this.scope_ = $scope;
 
   var vector = new ol.layer.Vector({
@@ -62,7 +85,35 @@ app.MainController = function($scope, ngeoFeatures, ngeoToolActivateMgr) {
 
   var dummyToolActivate = new ngeo.ToolActivate(this, 'dummyActive');
   ngeoToolActivateMgr.registerTool('mapTools', dummyToolActivate, true);
+
+  console.log(vector.getSource());
+  this.features = vector.getSource().getFeatures();
+
 };
 
+app.module.constant('ngeoExportFeatureFormats', [
+    ngeo.FeatureHelper.FormatType.KML,
+    ngeo.FeatureHelper.FormatType.GPX
+]);
+
+/**
+ * @export
+ */
+app.MainController.prototype.exportAsKml = function() {
+  
+  var kml = this.kmlFormat_.writeFeatures(this.features);
+
+  var charset = this.$document_.characterSet || 'UTF-8';
+  var type = 'application/vnd.google-earth.kml+xml;charset=' + charset;
+  var blob = new Blob([kml], {type: type});
+
+  saveAs(blob, 'file.kml');
+};
+
+app.MainController.prototype.exportAsJson = function() { 
+  var json = this.geoJsonFormat_.writeFeatures(this.features);
+  var blob = new Blob([json], {type: "application/json"});
+  saveAs(blob, 'fileJSON.json');
+};
 
 app.module.controller('MainController', app.MainController);
