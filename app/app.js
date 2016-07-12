53a7d56c3d8369212edff45df1bd5b44a59e8eab
MTA1711 <mongaachille@gmail.com>
2016-07-12 14:47:27 +0200

    Add delete feature

--- app/app.js
+++ app/app.js
@@ -5,6 +5,12 @@ var app = {};
 /** @type {!angular.Module} **/
 app.module = angular.module('app', ['ngeo']);
 
+var featuresSelected ;
+
+
+var DeleteFeatureActivate = 0;
+
+
 /**
  * @param {angular.$window} $window The Angular window service.
  * @return {angular.Directive} Directive Definition Object.
@@ -148,19 +154,111 @@ app.MainController = function($scope, $document, ngeoFeatures, ngeoToolActivateM
   this.interaction = new ngeo.interaction.Modify(
     /** @type {olx.interaction.ModifyOptions} */({
       features: this.vector_.getSource().getFeaturesCollection(),
-
-      removeCondition: function(event) {
-          console.log("deleteCondition call ");
+      deleteCondition: function(event) {
+          //console.log("deleteCondition call ");
+          console.log("deleteCondition call --->");
+          console.log(event);
           return ol.events.condition.altKeyOnly(event) &&
               ol.events.condition.singleClick(event);
       }
 
     }));
 
+  /*var modify = new ol.interaction.Modify({
+        features: features,
+        deleteCondition: function(event) {
+          console.log("deleteCondition call --->");
+          console.log(event);
+          return ol.events.condition.shiftKeyOnly(event) &&
+              ol.events.condition.singleClick(event);
+        }
+      }); */
+  /*this.features.on('singleclick', (function(evt) {
+    console.log("interaction event callback");
+    console.log(evt);
+  }) );*/
+
   var interaction = this.interaction;
   interaction.setActive(true);
   this.map.addInteraction(interaction);
 
+  /*var interactionSelect = new ol.interaction.Select({
+  });
+  this.map.addInteraction(interactionSelect);
+
+  interactionSelect.on('select', function (event) {
+     featuresSelected = event.target.getFeatures();
+  });*/
+
+  this.map.on('pointermove', function(e) {
+    if (e.dragging) return;
+    var pixel = this.getEventPixel(e.originalEvent);
+    var hit = this.hasFeatureAtPixel(pixel);
+    
+    this.getTarget().style.cursor = hit ? 'pointer' : '';
+  });
+
+
+
+  this.map.on('singleclick', function(e) {
+      if (DeleteFeatureActivate == 1){
+      console.log (e.coordinate);
+      console.log (e.pixel);
+
+      var Coordinate = e.coordinate ;
+      var Pixel = e.pixel ;
+
+      if (this.hasFeatureAtPixel(Pixel)){
+        console.log (" Features find");
+        //console.log (vector);
+        var Feature = vector.getSource().getClosestFeatureToCoordinate(Coordinate);
+        var TypeFeature = Feature.getGeometry().getType();
+        console.log ( TypeFeature ) ;
+
+        if ( TypeFeature == "Point"){
+          console.log (" remove point ");
+          vector.getSource().removeFeature(Feature);
+        }else if( TypeFeature == "LineString"){
+          console.log (" remove point on LineString ");
+
+          if (Feature.getGeometry().getLength() > 2 ){
+            //var PointInLine = Feature.getGeometry().getClosestPoint(Coordinate);
+            //console.log (PointInLine);
+            var PrevCoordinates = Feature.getGeometry().getCoordinates();
+            console.log (PrevCoordinates);
+
+            var PointInLine = getClosestPoint(Coordinate, PrevCoordinates);
+            //console.log (PointInLine);
+
+            var NewCoordinates = [];
+            var j = 0;
+            for (i in PrevCoordinates){
+               if (PrevCoordinates[i][0] == PointInLine[0] && PrevCoordinates[i][1] == PointInLine[1]){
+                 console.log("continue");
+                 continue;
+               }else{
+                 console.log("no continue");
+                 NewCoordinates[j++] = PrevCoordinates[i];
+               }
+            }
+            console.log(NewCoordinates);
+            Feature.getGeometry().setCoordinates(NewCoordinates);
+          }else{
+            vector.getSource().removeFeature(Feature);
+          }
+ 
+          
+        }
+
+
+      }else{
+        console.log (" No features ");
+      }
+    }else{
+      console.log("delete element desactivated");
+    }
+
+  });
 };
 
 
@@ -182,6 +280,7 @@ app.MainController.prototype.exportAsKml = function() {
 app.MainController.prototype.exportAsJson = function() { 
   console.log(this.vector_.getSource().getFeaturesCollection());
   var json = this.geoJsonFormat_.writeFeatures(this.features);
+  console.log(json);
   var blob = new Blob([json], {type: "application/json"});
   saveAs(blob, 'fileJSON.json');
 };
@@ -206,8 +305,52 @@ app.MainController.prototype.importJSON_ = function(jsonFile) {
 };
 
 
+app.MainController.prototype.removeSelectedFeatures = function (){
+  //this.vector_.getSource().getFeaturesCollection().removeAt(0);
+  this.vector_.getSource().getFeaturesCollection().clear();
+
+  /* if ( typeof featuresSelected != 'undefined' ){
+    //console.log(featuresSelected);
+    //console.log(featuresSelected.getLength());
+    for (x in featuresSelected.getArray()){
+      a = featuresSelected.getArray()[x] ;
+      console.log (a.getId());
+      console.log(this.vector_.getSource().getFeaturesCollection());
+      this.vector_.getSource().getFeaturesCollection().remove(a);
+    }*
+   }*/
+}
+
 app.module.controller('MainController', app.MainController);
 
 $(function() {
   $( "#controlPanel" ).draggable();
 });
+
+
+function DeletePointOnMap(MyMap, Pixel){
+  if (MyMap.hasFeatureAtPixel(pixel)){
+
+  }else{
+    console.log (pixel + " No features ");
+  }
+}
+
+
+function DistanceBetweenPoints(  a ,  b){
+  return  Math.sqrt ( ( a[0] - b[0] ) * ( a[0] - b[0] ) + ( a[1] - b[1] )*( a[1] - b[1] ) ) ;
+}
+
+function getClosestPoint( Point, ArrayOfPoint){
+  var PositionOfClosestPoint = 0;
+  var MinimunDistance = Number.MAX_SAFE_INTEGER ;
+  for (i in ArrayOfPoint){
+     var d = DistanceBetweenPoints ( Point, ArrayOfPoint[i]) ;
+     if (d < MinimunDistance) {
+       MinimunDistance = d ;
+       PositionOfClosestPoint = i ;
+     }
+  }
+  console.log(MinimunDistance);
+  return ArrayOfPoint[PositionOfClosestPoint];
+}
